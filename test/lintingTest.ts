484911d294ee119b4e43f78d813a5b0f9b4fea58
Jessica Kerr <jessitron@gmail.com>
2017-12-26 14:48:03 -0800

    skip test

--- test/lintingTest.ts
+++ test/lintingTest.ts
@@ -2,7 +2,7 @@ import * as stringify from "json-stringify-safe";
 import "mocha";
 import { runTslint } from "../src/handlers/PushToTsLinting";
 
-describe("can we lint the thing?", () => {
+describe.skip("can we lint the thing?", () => {
 
     it("can lint a thing", done => {
         runTslint("/Users/jessitron/code/atomist/upgrade-client-automation")
