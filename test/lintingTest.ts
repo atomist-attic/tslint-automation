d98e6e019a7960efba8496518cff9a4284747bdf
Jessica Kerr <jessitron@gmail.com>
2017-12-26 14:54:38 -0800

    docker service

--- test/lintingTest.ts
+++ test/lintingTest.ts
@@ -1,16 +1,21 @@
 import * as stringify from "json-stringify-safe";
+import * as assert from "power-assert";
 import "mocha";
 import { runTslint } from "../src/handlers/PushToTsLinting";
+import { GitCommandGitProject } from "@atomist/automation-client/project/git/GitCommandGitProject";
+import { GitHubRepoRef } from "@atomist/automation-client/operations/common/GitHubRepoRef";
+
+const GitHubCredentials = { token: process.env.GITHUB_TOKEN };
 
 describe.skip("can we lint the thing?", () => {
 
     it("can lint a thing", done => {
-        runTslint("/Users/jessitron/code/atomist/upgrade-client-automation")
-            .then(
-                result => {
-                    console.log("what");
-                    console.log(stringify(result));
-                })
+        GitCommandGitProject.cloned(GitHubCredentials, new GitHubRepoRef("atomist", "automation-client-ts"))
+            .then(project => runTslint(project.baseDir))
+            .then(result => {
+                assert(result.success);
+                console.log(stringify(result));
+            })
             .then(() => done(), done);
     }).timeout(100000);
 });
