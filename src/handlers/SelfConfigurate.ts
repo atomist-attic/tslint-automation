89ec8567aea1a1d2f7e66ce9a21b138c3c858771
Jessica Kerr <jessitron@gmail.com>
2017-12-28 18:43:59 -0600

    wow this got complicated

--- src/handlers/SelfConfigurate.ts
+++ src/handlers/SelfConfigurate.ts
@@ -1,14 +1,18 @@
 import {
+    CommandHandler, Failure,
     HandleCommand, HandlerContext, HandlerResult, MappedParameter, MappedParameters, Parameter, Secret, Secrets,
     Success,
 } from "@atomist/automation-client";
 import { Parameters } from "@atomist/automation-client/decorators";
 import { GitHubRepoRef } from "@atomist/automation-client/operations/common/GitHubRepoRef";
-import { failedEdit, ProjectEditor, successfulEdit } from "@atomist/automation-client/operations/edit/projectEditor";
+import { EditResult, failedEdit, ProjectEditor, successfulEdit } from "@atomist/automation-client/operations/edit/projectEditor";
 import { GitCommandGitProject } from "@atomist/automation-client/project/git/GitCommandGitProject";
 import { Project } from "@atomist/automation-client/project/Project";
 import * as slack from "@atomist/slack-messages/SlackMessages";
 import { adminCreds, adminSlackUserNames } from "../atomist.config";
+import { GitStatus } from "@atomist/automation-client/project/git/gitStatus";
+import * as stringify from "json-stringify-safe";
+import * as _ from "lodash";
 
 const whereToAdd = /PeopleWhoDoNotWantMeToOfferToHelp *= *\[/;
 
@@ -36,31 +40,112 @@ export class StopBotheringMeParams {
 
 }
 
-function initialReport(ctx: HandlerContext, parameters: StopBotheringMeParams) {
-    return ctx.messageClient.addressUsers(`Sad day: ${slack.user(parameters.screenName)}`, adminSlackUserNames);
+const MyGitHubOrganization = "atomist";
+const MyGitHubRepository = "tslint-automation";
+
+interface Analysis {
+    pushed: boolean,
+    editResult?: EditResult,
+    messageId: string,
+    sha?: string,
+    error?: Error,
+    message?: string
 }
 
+@CommandHandler("Stop offering to help the invoking user", "stop offering to help with linting errors")
 export class StopBotheringMe implements HandleCommand<StopBotheringMeParams> {
-    public handle(ctx: HandlerContext, parameters: StopBotheringMeParams): Promise<HandlerResult> {
+    public handle(context: HandlerContext, parameters: StopBotheringMeParams): Promise<HandlerResult> {
         const messageId = "stop-bothering-" + parameters.screenName;
+        const reportError = reportErrorFunction(context, parameters);
         // someday, parse reporef from package json
-        return initialReport(ctx, parameters).then(() =>
-        GitCommandGitProject.cloned(adminCreds, new GitHubRepoRef("atomist", "tslint-automation")))
-            .then(project => addPersonWhoDoesNotWantMeToOfferToHelp(parameters.screenName)(project, ctx)
-                .then(editResult => {
-                    if (editResult.success && editResult.edited) {
-                        return project.commit(`${parameters.screenName} doesn't want me to offer to help
+        return initialReport(context, parameters).then(() =>
+            GitCommandGitProject.cloned(adminCreds, new GitHubRepoRef(MyGitHubOrganization, MyGitHubRepository)))
+            .then(project => addPersonWhoDoesNotWantMeToOfferToHelp(parameters.screenName)(project, context)
+                    .then(editResult => {
+                            if (editResult.success && editResult.edited) {
+                                return project.commit(`${parameters.screenName} doesn't want me to offer to help
 
 [atomist:${messageId}]`)
-                            .then(() => project.push());
-                    } else {
-
-                    }
-                }).then(() => Success));
+                                    .then(() => project.push(), reportError("commit failed"))
+                                    .then(() => project.gitStatus(), reportError("push failed"))
+                                    .then((gs: GitStatus) => reportProgress(context, messageId, { sha: gs.sha }),
+                                        reportError("git status failed"))
+                                    .then(() => ({ pushed: true, editResult, messageId }));
+                            } else {
+                                return Promise.resolve({ pushed: false, editResult, messageId })
+                            }
+                        },
+                        reportError("editor threw an exception"))
+                    .then(analysis => finalReport(context, parameters, analysis)),
+                reportError("Failed to clone"))
+            .then(() => Success, error => Failure);
     }
 
     public freshParametersInstance(): StopBotheringMeParams {
         return new StopBotheringMeParams();
     }
 
+}
+
+function initialReport(context: HandlerContext, parameters: StopBotheringMeParams) {
+    return context.messageClient.addressUsers(`Sad day: ${parameters.screenName} invoked StopBotheringMe.`, adminSlackUserNames, { id: context.correlationId });
+}
+
+function reportProgress(context: HandlerContext, messageId: string, details: { sha: string }) {
+    return context.messageClient.respond(
+        `I have changed my programming to avoid offering to help with your linting errors in the future.
+:white_check_mark: ${linkToCommit(details)}
+:empty-orange-square: Build
+:empty-orange-square: Deploy`,
+        { id: messageId })
+}
+
+function linkToCommit(details: { sha?: string }): string {
+    if (details.sha) {
+        return slack.url(
+            `https://github.com/${MyGitHubOrganization}/${MyGitHubRepository}/commit/${details.sha}`,
+            "Code change");
+    } else {
+        return "(no commit sha)";
+    }
+}
+
+function reportErrorFunction(context: HandlerContext, parameters: StopBotheringMeParams) {
+    return (message: string) => (error: Error) => {
+        const attachment: slack.Attachment = {
+            fallback: "report",
+            color: "#bb2510",
+            fields: fields(["message"], ["error"],
+                {  error, message }),
+        };
+        const slackMessage: slack.SlackMessage = {
+            text: `${parameters.screenName} invoked StopBotheringMe.`,
+            attachments: [attachment],
+        };
+        return context.messageClient.addressUsers(slackMessage, adminSlackUserNames, { id: context.correlationId })
+            .then(() => Promise.reject(error));
+    }
+}
+
+function finalReport(context: HandlerContext, parameters: StopBotheringMeParams,
+                     analysis: Analysis) {
+    const attachment: slack.Attachment = {
+        fallback: "report",
+        color: "#20aa00",
+        fields: fields(["edited", "messageId"], ["commit"],
+            {  ...analysis, commit: linkToCommit(analysis),
+                edited: analysis.editResult.edited,}),
+    };
+    const message: slack.SlackMessage = {
+        text: `${parameters.screenName} invoked StopBotheringMe.`,
+        attachments: [attachment],
+    };
+    return context.messageClient.addressUsers(message, adminSlackUserNames, { id: context.correlationId })
+}
+
+function fields(shortOnes: string[], longOnes: string[], source: object) {
+    const shorts = shortOnes.map(f => ({ title: f, value: stringify(_.get(source, f, "undefined")), short: true }));
+    const longs = longOnes.map(f => ({ title: f, value: stringify(_.get(source, f, "undefined")), short: false }));
+
+    return shorts.concat(longs);
 }
