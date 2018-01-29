9fedec38f5c21da70c3e97c4dfc87b883bb698d2
Jessica Kerr <jessitron@gmail.com>
2018-01-29 14:39:29 -0400

    Revert "upgrade libs"
    This reverts commit 2afad79ddaaf681937ef706032324f6a5514eb78.

--- src/handlers/PushToTsLinting.ts
+++ src/handlers/PushToTsLinting.ts
@@ -498,7 +498,6 @@ export function runTslint(project: GitProject) {
         project: project.baseDir,
         format: "json",
         outputAbsolutePaths: false,
-        files: ["**/*.ts"]
     };
     const errors: string[] = [];
     const logs: string[] = [];
