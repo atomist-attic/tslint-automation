328cfd3ded80869fe9a4b4e813c77901b5867b69
David Dooling <david@atomist.com>
2018-03-22 08:53:01 -0500

    Upgrade dependencies, revamp config & kube
    Upgrade to @atomist/automation-client@0.9.0 and leverage its
    configuration capabilities to simplify configuration of this client.
    
    Update other dependencies, including tslint, and move typescript to
    production dependency.
    
    Add logzio logging.
    
    Remove self deployment code #6.
    
    Update Dockerfile to use more memory, only install production
    dependencies, and remove gcloud installation.
    
    Update README and kubernetes spec #2.
    
    security: SecKeychainSearchCopyNext: The specified item could not be found in the keychain.

--- Dockerfile
+++ Dockerfile
@@ -8,29 +8,22 @@ RUN curl -s -L -O https://github.com/Yelp/dumb-init/releases/download/v$DUMB_INI
     && dpkg -i dumb-init_${DUMB_INIT_VERSION}_amd64.deb \
     && rm -f dumb-init_${DUMB_INIT_VERSION}_amd64.deb
 
-# I need gcloud to deploy to GKE
-ENV PATH $PATH:/opt/gcloud/google-cloud-sdk/bin
-RUN curl -sSL https://sdk.cloud.google.com > /tmp/gcl && \
-    bash /tmp/gcl --install-dir=/opt/gcloud && \
-    gcloud --quiet components install kubectl
-
-
 RUN mkdir -p /opt/app
 
 WORKDIR /opt/app
 
-COPY . .
+EXPOSE 2866
 
 ENV NPM_CONFIG_LOGLEVEL warn
 
-RUN npm install
-
 ENV SUPPRESS_NO_CONFIG_WARNING true
 
-EXPOSE 2866
-
-ENTRYPOINT ["dumb-init", "node", "--trace-warnings", "--expose_gc", "--optimize_for_size", "--always_compact", "--max_old_space_size=128"]
+ENTRYPOINT ["dumb-init", "node", "--trace-warnings", "--expose_gc", "--optimize_for_size", "--always_compact", "--max_old_space_size=768"]
 
 CMD ["node_modules/@atomist/automation-client/start.client.js"]
 
 RUN  git config --global user.email "bot@atomist.com" && git config --global user.name "Atomist Bot"
+
+COPY . .
+
+RUN npm install --only=production
--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -1,22 +1,17 @@
-import { Configuration } from "@atomist/automation-client/configuration";
-import { adminCreds } from "./credentials";
+import { Configuration } from "@atomist/automation-client";
 import { deleteLineCommand } from "./handlers/BittyEditors/DeleteLine";
 import { insertAboveLineCommand } from "./handlers/BittyEditors/InsertAboveLine";
 import { replaceLineCommand } from "./handlers/BittyEditors/ReplaceLine";
 import { reportConfigurationCommand } from "./handlers/Configuration/ReportConfiguration";
 import { DoOfferToHelp, StopBotheringMe } from "./handlers/Configuration/SelfConfigurate";
-import { DeployAfterSuccessfulBuild, deployCommand } from "./handlers/DeploySelf";
 import { HelloWorld } from "./handlers/HelloWorld";
 import { PleaseLint, PushToTsLinting } from "./handlers/PushToTsLinting";
 import { replaceConsoleLogWithLoggerCommand } from "./handlers/specializedEditor";
-import { sshCommand } from "./handlers/Ssh";
+// import { sshCommand } from "./handlers/Ssh";
 import { UpdateMessageOnBuild } from "./handlers/UpdateMessageOnBuild";
-
-const AtomistCommunity = "T29E48P34";
-const teamIds = [process.env.ATOMIST_TEAM || AtomistCommunity];
+import { configureLogzio } from "./util/logzio";
 
 export const configuration: Configuration = {
-    teamIds,
     commands: [
         HelloWorld,
         StopBotheringMe,
@@ -24,7 +19,6 @@ export const configuration: Configuration = {
         PleaseLint,
         () => insertAboveLineCommand(),
         () => replaceLineCommand(),
-        () => deployCommand(),
         // () => sshCommand(),
         () => deleteLineCommand(),
         () => replaceConsoleLogWithLoggerCommand(),
@@ -32,18 +26,7 @@ export const configuration: Configuration = {
     ],
     events: [
         () => new PushToTsLinting(),
-        DeployAfterSuccessfulBuild,
         UpdateMessageOnBuild,
     ],
-    token: adminCreds.token,
-    cluster: {
-        enabled: true,
-    },
-    applicationEvents: {
-        teamId: AtomistCommunity,
-        enabled: true,
-    },
-    http: {
-        enabled: true,
-    },
+    postProcessors: [configureLogzio],
 };
