469a07c62fe147cb8e1649dafd25a40746d43264
David Dooling <david@atomist.com>
2018-04-25 17:00:39 -0500

    Update HelloAutomation, remove unused files
    Update the HelloWorld command handler to the more flexible, if a bit
    less informative, HelloAutomation.
    
    Remove unused Ssh handler and supporting files.

--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -20,22 +20,20 @@ import { insertAboveLineCommand } from "./handlers/BittyEditors/InsertAboveLine"
 import { replaceLineCommand } from "./handlers/BittyEditors/ReplaceLine";
 import { reportConfigurationCommand } from "./handlers/Configuration/ReportConfiguration";
 import { DoOfferToHelp, StopBotheringMe } from "./handlers/Configuration/SelfConfigurate";
-import { HelloWorld } from "./handlers/HelloWorld";
+import { HelloAutomation } from "./handlers/HelloAutomation";
 import { PleaseLint, PushToTsLinting } from "./handlers/PushToTsLinting";
 import { replaceConsoleLogWithLoggerCommand } from "./handlers/specializedEditor";
-// import { sshCommand } from "./handlers/Ssh";
 import { UpdateMessageOnBuild } from "./handlers/UpdateMessageOnBuild";
 import { configureLogzio } from "./util/logzio";
 
 export const configuration: Configuration = {
     commands: [
-        HelloWorld,
+        HelloAutomation,
         StopBotheringMe,
         DoOfferToHelp,
         PleaseLint,
         () => insertAboveLineCommand(),
         () => replaceLineCommand(),
-        // () => sshCommand(),
         () => deleteLineCommand(),
         () => replaceConsoleLogWithLoggerCommand(),
         () => reportConfigurationCommand,
